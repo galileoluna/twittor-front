0d404665add21e0cf8dedc17918bc5e3957eab9d
galileoluna <galileoungs@gmail.com>
2020-08-16 20:47:21 -0300

    estilizo el inicio

--- src/page/SignInSingUp/SignInSingUp.js
+++ src/page/SignInSingUp/SignInSingUp.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 import {Container, Row, Col, Button} from "react-bootstrap";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faSearch,faUsers, faComment } from "@fortawesome/free-solid-svg-icons";
+import LogoWhiteTwittor from "../../assets/png/logo-white.png";
+import LogoTwittor from "../../assets/png/logo.png";
+
+import "./SignInSingUp.scss";
 
 export default function SignInSingUp() {
     return (
@@ -14,15 +20,29 @@ export default function SignInSingUp() {
 }
 function LeftComponent(){
     return (
-        <Col className="signin-signup__left">
-            <h2>Left Component...</h2>
+        <Col className="signin-signup__left "xs={6}>
+          <img src={LogoTwittor} alt="Twittor" />
+          <div>
+              <h2>
+                  <FontAwesomeIcon icon={faSearch}/>
+                   Sigue lo que te interesa
+              </h2>
+              <h2>
+              <FontAwesomeIcon icon={faUsers}/>
+                  Enterate de que esta hablando la gente
+              </h2>
+              <h2>
+              <FontAwesomeIcon icon={faComment}/>
+                  Unete a la conversacion
+              </h2>
+          </div>
         </Col>
     )
 }
 
 function RightComponent(){
     return (
-        <Col className="signin-signup__right">
+        <Col className="signin-signup__right" xs={6}>
             <h2>Right Component...</h2>
         </Col>
     )
