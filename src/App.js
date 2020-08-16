92df045b17aa7a04e3f1d4643dd72a085086f10e
galileoluna <galileoungs@gmail.com>
2020-08-16 19:22:32 -0300

    creo proyecto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
+
+
+export default function App() {
+
+  //creo un nuevo estado
+  //var que guarda el estado
+  const [user, setUser] =useState({name:"gali"});
+
+
+//verifico que el useState tenga algun valor
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {user ? (<h1>Estas Logueado</h1>): (<h1>No estas Logueado</h1>)}
     </div>
-  );
+  )
+//cuando usuario tenga contenido
+
+
 }
 
-export default App;
+
